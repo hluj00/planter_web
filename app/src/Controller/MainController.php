70d189c4ea001fcd0de95f2d171bee53f5db8f0c
jhlubucek <honza.ucty@gmail.com>
2021-03-20 20:39:48 +0000

    commit

--- app/src/Controller/MainController.php
+++ app/src/Controller/MainController.php
@@ -2,21 +2,28 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\User;
 
 class MainController extends BaseController
 {
 
     /**
      * @Route("/main", name="main")
+     * @param UserRepository $userRepo
+     * @return Response
      */
-    public function index(): Response
+    public function index(UserRepository $userRepo): Response
     {
+//         = new UserRepository();
+//        $userRepo = $this->getDoctrine()->getRepository(User::class);
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
-            'kokos' => 'testič'
+            'kokos' => $userRepo->findOneById(1)->getEmail()
         ]);
     }
+
 }
