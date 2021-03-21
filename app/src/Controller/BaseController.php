179369e0e3aa198793431126274876f94b1d8abe
jhlubucek <honza.ucty@gmail.com>
2021-03-21 15:59:50 +0000

    register

--- app/src/Controller/BaseController.php
+++ app/src/Controller/BaseController.php
@@ -2,15 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BaseController extends AbstractController
 {
-
-
     /**
-     * @Route("/lucky/number", name="lucky_number")
+     * @return object|User|null
      */
+    protected function getUser()
+    {
+        return parent::getUser();
+    }
+
+
+
 }
--- app/src/Controller/LuckyController.php
+++ app/src/Controller/LuckyController.php
@@ -3,16 +3,23 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class LuckyController extends BaseController
 {
+    /**
+     * @Route("/lucky-number", name="lucky_number")
+     */
     public function number(): Response
     {
 
         $number = random_int(0, 100);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        //         = new UserRepository();
+//        $userRepo = $this->getDoctrine()->getRepository(User::class);
+        return $this->render('number/number.html.twig', [
+            'number' => $number,
+            'uuid' => $this->getUser()->getId()
+        ]);
     }
 }
